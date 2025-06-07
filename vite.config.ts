99af32696ab7ecac24282123ae1ac404fdd5c0a4
Sruthi P <sp@demandbase.com>
2025-06-07 21:12:55 +0530

    Fix Github Pages Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This will be your repository name for GitHub Pages
+  base: '/spoddutur/', // Update this to match your repository name
 })
